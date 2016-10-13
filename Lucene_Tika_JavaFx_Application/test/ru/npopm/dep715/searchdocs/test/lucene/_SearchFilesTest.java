dc1e529bb7aef851beabc0fa99e94278c2306c57
ajratb <ajratb@ya.ru>
2016-10-13 11:39:31 +0700

    add lucene_tika_javafx project. Im going to split it into 2 projects: lib and client

---
+++ Lucene_Tika_JavaFx_Application/test/ru/npopm/dep715/searchdocs/test/lucene/_SearchFilesTest.java
@@ -0,0 +1,38 @@
+/*
+ * ROSCOSMOS CORP. PROPERTY. 
+ * Don't use without permission
+ */
+package ru.npopm.dep715.searchdocs.test.lucene;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author ayrat
+ */
+public class _SearchFilesTest {
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+//     @Test
+//     public void hello() {}
+}
